a4dfc5b378b52c2dd0f56b965d71b8db4755fc8a
Kristopher Roller <akjroller@gmail.com>
2023-06-13 15:55:55 -0400

    fixed the .exe
    Fixed the file so when you launch the .exe you can move the window and resize it. This functionality was not working before.

--- diablo_gold.py
+++ diablo_gold.py
@@ -6,6 +6,7 @@ from mss import mss
 import time
 import re
 
+
 def select_screen_size():
     screen_sizes = {
         "1080p": {'top': 100, 'left': 400, 'width': 1020, 'height': 750}
@@ -15,29 +16,26 @@ def select_screen_size():
               [sg.Combo(list(screen_sizes.keys()), key='-SCREEN_SIZE-', size=(20, 1))],
               [sg.Button("OK")]]
 
-    window = sg.Window('Screen Size Selection', layout)
-    screen_size = None
+    window = sg.Window('Screen Size Selection', layout, resizable=True, grab_anywhere=True, icon='icons/d4gold.ico')
 
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'OK':
-            if event == 'OK':
-                screen_size = screen_sizes.get(values['-SCREEN_SIZE-'])
             break
 
     window.close()
-    return screen_size
+    return screen_sizes.get(values['-SCREEN_SIZE-'])
+
 
 def get_gold_from_screen(mon, sct):
     screenshot = np.array(sct.grab(mon))
     grayscale = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string(grayscale, config='--psm 11')
 
-
     gold_amount = 0
     if "Gold" in text:
         try:
-            gold_amount = int(re.findall(r'(\d+)\s*Gold', text)[0])
+            gold_amount = int(re.findall(r'Gold\s*(\d+)', text)[0])
             print(f"{gold_amount} gold found on screen.")
         except (IndexError, ValueError):
             print("Could not parse gold amount")
@@ -46,12 +44,6 @@ def get_gold_from_screen(mon, sct):
 
     return gold_amount
 
-def create_main_window():
-    layout = [[sg.Text('Estimated gold per hour:'), sg.Text(size=(15,1), key='-OUTPUT-')]]
-    return sg.Window('Diablo Gold Tracker', layout)
-
-def update_gui(window, message):
-    window['-OUTPUT-'].update(message)
 
 def main():
     mon = select_screen_size()
@@ -62,37 +54,36 @@ def main():
     sct = mss()
     gold_list = []
     weights = []
-    decay_rate = 0.9 
+    decay_rate = 0.9
+
+    layout = [[sg.Text("Estimated gold per hour: "), sg.Text(size=(15,1), key='-OUTPUT-')]]
+    window = sg.Window('Gold Per Hour', layout, finalize=True, resizable=True, grab_anywhere=True, size=(500, 500), icon='icons/d4gold.ico')
+
+
     start_time = time.time()
-
-    layout = [[sg.Text("Gold Detected:", size=(20,1), key='-GOLD_DETECTED-', justification='left')],
-              [sg.Text("Estimated gold per hour:", size=(20,1), key='-ESTIMATED_GPH-', justification='left')],
-              [sg.Button("Exit")]]
-
-
-    window = sg.Window('Diablo Gold Tracker', layout, finalize=True, keep_on_top=True)
 
     while True:
         gold_current_second = get_gold_from_screen(mon, sct)
         gold_list.append(gold_current_second)
         weights.append(decay_rate ** len(gold_list))
 
-        window['-GOLD_DETECTED-'].update(f"Gold Detected: {gold_current_second}")
-
-        if time.time() - start_time >= 1:
+        if time.time() - start_time >= 60:
             weights = [weight / sum(weights) for weight in weights]
 
             weighted_gold_per_minute = sum(g * w for g, w in zip(gold_list, weights))
             gold_per_hour = weighted_gold_per_minute * 60
 
-            window['-ESTIMATED_GPH-'].update(f"Estimated gold per hour: {gold_per_hour}")
+            print(f"Estimated gold per hour: {gold_per_hour}")
+            window['-OUTPUT-'].update(f'{gold_per_hour:.2f}')
 
             gold_list = []
             weights = []
             start_time = time.time()
+        else:
+            time.sleep(0.1)
 
-        event, values = window.read(timeout=100)
-        if event == sg.WINDOW_CLOSED or event == 'Exit':
+        event, values = window.read(timeout=10)
+        if event == sg.WIN_CLOSED:
             break
 
     window.close()
